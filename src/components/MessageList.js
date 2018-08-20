e077e108db8c2c5f14999236e88af4eeea255edb
David Miller <hawky454@gmail.com>
2018-08-20 11:18:28 -0500

    created components folder and have basic layout of app no functionality yet

---
+++ src/components/MessageList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
+class MessageList extends React.Component {
+    render() {
+        return (
+            <div className="message-list">
+                
+            </div>
+        )
+    }
+}
+
+export default MessageList
