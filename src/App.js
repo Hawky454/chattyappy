c430c672d5f3cb530be0be981e3a7fa7c0ec8a2a
David Miller <hawky454@gmail.com>
2018-09-04 09:25:33 -0500

    got to a stopping point and now starting with quarter 4

--- src/App.js
+++ src/App.js
@@ -4,15 +4,51 @@ import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 import './App.css'
+import Chatkit from '@pusher/chatkit'
+import { tokenUrl, instanceLocator, admin } from './config'
 
+//*note - state is private to each component while props is not, props can be shared between conponents
 
 class App extends React.Component {
 
+constructor() {
+  super()
+  this.state = {
+    messages: []
+  }
+}
+
+  //! life cycle method called componentDidMount is used to hook up to the chatkit api
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: admin,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+    //! this is the actual fetch call that returns a promise
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: 14328009,
+        hooks: {
+          onNewMessage: mmmessage => {
+            this.setState({
+              messages: [...this.state.messages, mmmessage ]
+            })
+          }
+        }
+      })
+    })
+  }
+
   render() { 
+    console.log(this.state)
     return ( 
       <div className="app">
         <RoomList />
-        <MessageList />
+        <MessageList messagesProp={this.state.messages}/>
         <SendMessageForm />
         <NewRoomForm />
       </div>
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,41 +1,12 @@
 import React from 'react'
 
-const dummyData = [
-    {
-        senderId: 'perborgen',
-        text: 'Hey, how is it going?'
-    },
-    {
-        senderId: 'janedoe',
-        text: 'Great! How about you? You\'re as smooth as Tennessee Whisky'
-    },
-    {
-        senderId: 'perborgen',
-        text: 'Good to hear! I am great as well, although I am having a hard time figureing out how to set up the css for this text part!!!'
-    },
-    {
-        senderId: 'David Miller',
-        text: 'Ya see, this is all just an object and you are now learning how to use Object Oriented Programming.... good job!'
-    },
-    {
-        senderId: 'Davina',
-        text: 'Oh mister, you are so funny!'
-    },
-    {
-        senderId: 'David Miller',
-        text: 'I know, I know'
-    }
-]
-
-
-
-
 class MessageList extends React.Component {
+    
 
     render() { 
         return ( 
             <div className="message-list">
-              {dummyData.map((data, index) => {
+              {this.props.messagesProp.map((data, index) => {
                   return (
                         <section 
                            className="message"
